4b08a5f0f9e919f2a0b0c38edf7a749322edb0ff
SD <zswdzly@outlook.com>
2017-09-16 23:53:04 +0800

    Create BIT.cpp

---
+++ BIT.cpp
@@ -0,0 +1,38 @@
+#include<cstdio>
+int n,m,c[500070],a[500070];
+int lowbit(int i){
+    return i&-i;
+}
+void add(int x,int d){
+    while(x<=n){
+        c[x]+=d;
+        x+=lowbit(x);
+    }
+}
+int sum(int x){
+    int s=0;
+    while(x){
+        s+=c[x];
+        x-=lowbit(x);
+    }
+    return s;
+}
+int main(){
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<=n;i++){
+        scanf("%d",&a[i]);
+        add(i,a[i]);
+    }
+    for(int i=1;i<=m;i++){
+        int c;scanf("%d",&c);
+        if(c == 1){
+            int x,k;
+            scanf("%d%d",&x,&k);
+            add(x,k);
+        }else{
+            int x,y;scanf("%d%d",&x,&y);
+            printf("%d\n",sum(y)-sum(x-1));
+        }
+    }
+    return 0;
+}
