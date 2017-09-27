6bb616cb24971902f0496f8030b833772d1607d4
SD <zswdzly@outlook.com>
2017-09-27 19:09:46 +0800

    Update forestar.cpp

--- forestar.cpp
+++ forestar.cpp
@@ -2,28 +2,36 @@
 #include<cstring>
 #include<iostream>
 using namespace std;
-int head[1008611],k=0;
+int head[1008611],k=1;
 int a=0,b=0,c=0;
+int s[10086];
 struct edge{
 	int to,next,w;
 };
 edge e[1008611];
 inline void addedge(int u,int v,int w){
 	e[k]=(edge){v,head[u],w};
-	head[u]=k;
-	k++;
+	head[u]=k++;
 }
-inline void dfs(int x){
-	for(int i=0;i<k;i++)    
-        for(int j=head[i];j!=-1;j=e[j].next)    
-            cout<<i<<" "<<e[j].to<<" "<<e[j].w<<endl;  
+void dfs(int p){
+    s[p]=1;
+    printf("%d|",p);
+    for(int i=head[p];i!=-1;i=e[i].next){
+        int v=e[i].to;
+        if(s[v]==1) continue;
+        else dfs(v);
+    }
 }
 int main(){
+	freopen("data.in","r",stdin);
 	memset(head,-1,sizeof(head));
+	memset(s,0,sizeof(s));
 	while(a!=-1){
 		scanf("%d%d%d",&a,&b,&c);
 		addedge(a,b,c);
+		addedge(b,a,c);
 	}
-	dfs(3);
+	dfs(1);
+	fclose(stdin);
 	return 0;
 }
