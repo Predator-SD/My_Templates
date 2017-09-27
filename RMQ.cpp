2a3b3f91b193fc304494c8e172c6721db3a0e56c
SD <zswdzly@outlook.com>
2017-09-27 19:58:54 +0800

    Update RMQ.cpp

--- RMQ.cpp
+++ RMQ.cpp
@@ -8,11 +8,11 @@ void init(const vector<int> A){
     for(int i=0;i<n;i++) d[i][0]=A[i];
     for(int j=1;(1<<j)<=n;j++)
         for(int i=0;i+(1<<j)-1<n;i++)
-            d[i][j]=min(d[i][j-1],d[i+(1<<j-1)][j-1]);
+            d[i][j]=min(d[i][j-1],d[i+(1<<(j-1))][j-1]);
 }
 int RMQ(int L,int R){
     int k=0;
-    while((1<<k)<R-L+1) k++;   //可以打表 
+    while((1<<k)<R-L+1) k++;
     return min(d[L][k],d[R-(1<<k)+1][k]);
 }
 
