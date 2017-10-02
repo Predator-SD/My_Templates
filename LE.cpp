cddc9960126278a276c004f625e5c1bc0264dd43
SD <zswdzly@outlook.com>
2017-10-01 23:48:13 -0500

    欧拉筛

---
+++ LE.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+#define M 3000500  
+int p[400010], pNum;  
+bool f[M];  
+void LE(){  
+    int i,j;  
+    for(i=2;i<M;i++){  
+        if(!f[i]) p[pNum++]=i;  
+        for(j=0;j<pNum&&p[j]*i<M;j++){  
+            f[p[j]*i]=1;
+            if(!(i%p[j])) break;  
+        }  
+    }  
+}  
