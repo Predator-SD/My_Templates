8e793813afbf1b4a886ef62256ceee8a77aff11d
SD <zswdzly@outlook.com>
2017-10-21 08:38:20 -0500

    Update qread.cpp

--- qread.cpp
+++ qread.cpp
@@ -1,18 +1,5 @@
-#include<cstdio>
-const int MAXN = 10000000;
-const int MAXS = 60*1024*1024;
-int numbers[MAXN];
-char buf[MAXS];
-void analyse(char *buf,int len = MAXS){
-    int i;
-    numbers[i=0]=0;
-    for (char *p=buf;*p && p-buf<len;p++)
-        if (*p == ' ') numbers[++i]=0;
-        else numbers[i] = numbers[i] * 10 + *p - '0';
-}
-void fread_analyse(){
-    freopen("data.txt","rb",stdin);
-    int len = fread(buf,1,MAXS,stdin);
-    buf[len] = '\0';
-    analyse(buf,len);
+template<typename T>
+inline void read(T &x){
+  char ch;while((ch=getchar()),(ch>'9'||ch<'0'));
+  x=ch-'0';while((ch=getchar()),(ch>='0'&&ch<='9')) x=x*10+ch-'0';
 }
