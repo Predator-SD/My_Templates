efa92f885a63d4198d6bf4a724e2b7bc5a50c8f8
SD <zswdzly@outlook.com>
2017-10-22 17:58:47 +0800

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,8 +1,16 @@
 #include<bits/stdc++.h>
-#define MAXN 1008611
+#define N 1008611
 #define lowbit(x) x&-x
 using namespace std;
-int arr[MAXN];
-inline int query(int x){int res=0;while(x)res+=arr[x],x-=lowbit(x);return res;}  
-inline void add(int x,int n){while(x<MAXN)arr[x]+=n,x+=lowbit(x);}  
-inline int update(int x,int y,int n){add(x,n);add(y+1,-n);}  
+template<typename T>
+struct BIT{
+    struct SEMI{  
+        T arr[N];  
+        void add(int x,T n){while(x<=N)arr[x]+=n,x+=lowbit(x);}  
+        T sum(int x){int sum=0;while(x)sum+=arr[x],x-=lowbit(x);return sum;}  
+    }T1,T2;
+    void add(int x,T n){T1.add(x,n);T2.add(x,x*n);}  
+    void update(int L,int R,T n){add(L,n);add(R+1,-n);}  
+    T sum(int x){return (x+1)*T1.sum(x)-T2.sum(x);}  
+    T query(int L,int R){return sum(R)-sum(L-1);}
+};
