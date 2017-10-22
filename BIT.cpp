6b4767979576dbfa6db50b3126fb50c611780ced
SD <zswdzly@outlook.com>
2017-10-22 17:37:06 +0800

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,38 +1,8 @@
-#include<cstdio>
-int n,m,c[500070],a[500070];
-inline int lowbit(int i){
-    return i&-i;
-}
-inline void add(int x,int d){
-    while(x<=n){
-        c[x]+=d;
-        x+=lowbit(x);
-    }
-}
-inline int sum(int x){
-    int s=0;
-    while(x){
-        s+=c[x];
-        x-=lowbit(x);
-    }
-    return s;
-}
-int main(){
-    scanf("%d%d",&n,&m);
-    for(int i=1;i<=n;i++){
-        scanf("%d",&a[i]);
-        add(i,a[i]);
-    }
-    for(int i=1;i<=m;i++){
-        int c;scanf("%d",&c);
-        if(c == 1){
-            int x,k;
-            scanf("%d%d",&x,&k);
-            add(x,k);
-        }else{
-            int x,y;scanf("%d%d",&x,&y);
-            printf("%d\n",sum(y)-sum(x-1));
-        }
-    }
-    return 0;
-}
+#include<bits/stdc++.h>
+#define MAXN 1008611
+#define lowbit(x) x&-x
+using namespace std;
+int arr[MAXN];
+inline int query(int x){int res=0;while(x)res+=arr[x],x-=lowbit(x);return res;}  
+inline void add(int x,int n){while(x<MAXN)arr[x]+=n,x+=lowbit(x);}  
+inline int update(int x,int y,int n){add(x,n);add(y+1,-n);}  
