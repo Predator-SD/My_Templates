9e38b7a332ac906e3866aa608159163456694642
SD <zswdzly@outlook.com>
2017-12-11 07:19:28 +0800

    Update LE.cpp

--- 平凡/LE.cpp
+++ 平凡/LE.cpp
@@ -1,15 +1,15 @@
 #include<cstdio>
 int tot,prime[1008611],phi[1008611];
 bool mark[1008611];
-void euler(int N){
+inline void euler(int N){
 	phi[1]=1;
 	int i,j;
-	for(i=2;i<=N;i++){
+	for(i=2;i<=N;++i){
 		if(!mark[i]){
 		    prime[++tot]=i;
 		    phi[i]=i-1;
 	    }
-	    for(j=1;i<=tot;j++){
+	    for(j=1;j<=tot;++j){
 	    	if(i*prime[j]>N) break;
 	    	mark[i*prime[j]]=true;
 	    	if(!(i%prime[j])){
