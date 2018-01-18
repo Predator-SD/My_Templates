530123637be3664bedca994beb849d61d34d9df2
SD <zswdzly@outlook.com>
2018-01-18 09:51:22 +0800

    Update Suffix_Array.cpp

--- 平凡/Suffix_Array.cpp
+++ 平凡/Suffix_Array.cpp
@@ -1,79 +1,68 @@
 #include<bits/stdc++.h>
-#define uint unsigned int
-#define maxn 1008611
 using namespace std;
 
-namespace Suffix_Array{
-	char sss[maxn];
-	int s[maxn];
-	int sa[maxn],fir[maxn],sec[maxn],c[maxn],n;
-	int rank[maxn],height[maxn];
-	int SIGMA_SIZE=26;
+template<size_t N>
+class Suffix_Array{
+public:
+	int n;
+	int sa[N+5],s[N+5];
+	int ju[N+5];
 	
-	inline int idx(char x){
-		return x-'a';
+	struct bunch{
+		int id,alpha,beta;
+	}buc[N+5];
+	
+	inline static bool cmp(const bunch &lhs,const bunch &rhs){
+		return (lhs.alpha<rhs.alpha)||(lhs.alpha==rhs.alpha&&lhs.beta<rhs.beta);
 	}
 	
-	inline void init_s(char *sss){
-		for(uint i=0;i<strlen(sss);++i) s[i]=idx(sss[i]);
+	inline int idx(char s){
+		if(s>='0'&&s<='9') return 1+s-'0';
+		if(s>='A'&&s<='Z') return 11+s-'A';
+		if(s>='a'&&s<='z') return 37+s-'a';
+		return 63;
 	}
 	
-	inline void getSA(int sig=SIGMA_SIZE){
-		--sig;
-		int i,*x=fir,*y=sec;
-		
-		//for(i=0;i<sig;++i) c[i]=0;
-		for(i=0;i<n;++i) ++c[x[i]=s[i]];
-		for(i=1;i<sig;++i) c[i]+=c[i-1];
-		for(i=n-1;i<=0;--i) sa[--c[x[i]]]=i;
-		
+	inline void read(char in[]){
+		n=strlen(in);
+		for(int i=0;i<n;++i) s[i]=idx(in[i]);
+	}
+	
+	void getSA(){
+		bool flag=true;
 		for(int k=1;k<=n;k<<=1){
-			int p=0;
+			memset(buc,0,sizeof buc);
+			memset(ju,0,sizeof ju);
 			
-			for(i=n-k;i<n;++i) y[p++]=i;
-			for(i=0;i<n;++i) if(sa[i]>=k) y[p++]=sa[i]-k;
+			for(int cur1=0,cur2=k;cur1<n;++cur1,++cur2)
+				buc[cur1].id=cur1,
+				buc[cur1].alpha=s[cur1],
+				buc[cur1].beta=(cur2>=n?0:s[cur2]);
+			sort(buc,buc+n,cmp);
 			
-			for(i=0;i<sig;++i) c[i]=0;
-			for(i=0;i<n;++i) ++c[x[y[i]]];
-			for(i=0;i<sig;++i) c[i]+=c[i-1];
-			for(i=n-1;i>=0;--i) sa[--c[x[y[i]]]]=y[i];
+			for(int i=0,cnt=1;i<n;++i)
+				cnt+=((i!=0)&&(buc[i].alpha!=buc[i-1].alpha||buc[i].beta!=buc[i-1].beta)),
+				s[buc[i].id]=cnt;
 			
-			swap(x,y);
-			p=1;x[sa[0]]=0;
-			for(i=1;i<n;++i)
-				x[sa[i]]=((y[sa[i-1]]==y[sa[i]])&&(y[sa[i-1]+k]==y[sa[i]+k]))?p-1:p++;
-			if(p>=n) break;
-			sig=p;
+			for(int i=0;i<n;++i)
+				if(ju[s[i]]==0) ++ju[s[i]];
+				else{flag=false; break;}
+			if(flag==true) break;
+			else flag=true;
 		}
-	}
-	
-	inline int cmp(char* pattern,int p){
-		return strncmp(pattern,sss+sa[p],strlen(pattern));
-	}
-	
-	inline int find(char *P){
-		if(cmp(P,0)<0) return -1;
-		if(cmp(P,n-1)>0) return -1;
-		int l=0,r=n-1,mid,ans;
-		while(r>=l){
-			mid=(l+r)>>1;
-			ans=cmp(P,mid);
-			if(!ans) return mid;
-			if(ans<0) r=mid-1; else l=mid+1;
-		}
-		return -1;
-	}
-	
-	inline void getHeight(){
-		int i,j,k=0;
 		
-		for(i=0;i<n;++i) rank[sa[i]]=i;
-		
-		for(i=0;i<n;++i){
-			if(k) --k;
-			j=sa[rank[k]-1];
-			while(s[i+k]==s[j+k]) ++k;
-			height[rank[i]]=k;
-		}
+		for(int i=0;i<n;++i) sa[s[i]]=i+1;
 	}
+};
+
+Suffix_Array<1000005> sss;
+
+char a[1000005];
+
+int main(){
+	scanf("%s",a);
+	sss.read(a);
+	sss.getSA();
+	for(register int i=1;i<=sss.n;++i) printf("%d ",sss.sa[i]);
+	return 0;
 }
